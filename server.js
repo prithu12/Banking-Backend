3c640de0d41014bf29542d8317b969ffba63e232
prithu12 <prithur045@gmail.com>
2026-04-21 00:44:58 +0530

    Docker

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 const connectDB=require('./src/config/db');
 
 connectDB();
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000');
+const PORT=process.env.PORT || 3000;
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
 });
